de08c0eedef16979f860bea9fc51665e665e518d
Arfurboy <arfurboy@gmail.com>
2014-12-01 21:05:53 -0800

    move X with buttons

--- client/app/game/main.controller.js
+++ client/app/game/main.controller.js
@@ -3,4 +3,5 @@
 angular.module('mvpApp')
   .controller('GameCtrl', function ($scope, Game) {
     angular.extend($scope, Game);    
+    $scope.game.initRobot(1,5);
   });
--- client/app/game/main.factory.js
+++ client/app/game/main.factory.js
@@ -2,19 +2,68 @@
 
 angular.module('mvpApp')
   .factory('Game', function () {
+    var robot = {x:0 ,y:0};
     var grid = [];
-    for(var i=0; i<16; i++){
-      grid[i] = [];
-    }
-    for(var i=0; i<16; i++){
-      for(var j=0; j<16; j++){
-        grid[i][j] = '0';
+    var length = 16;
+
+    // set up board
+    // any even = wall
+    // both odds = space
+    for(var i=0; i<length; i++){
+      grid[i] = []
+      for(var j=0; j<length; j++){
+        grid[i][j] = null;
       }
     }
 
-    grid[1][2] = '<img src="assets/images/robo.png" alt="robo" height="20px">';
-    grid[1][3] = '<svg height="20"><circle fill="red"/></svg>';
+    var getGrid = function(){
+      return grid;
+    };
+
+    var initRobot = function(x,y){
+      if(x && y){
+        robot.x = x;
+        robot.y = y;
+      }
+      grid[ robot.y ][ robot.x ] = 'X';
+    };
+
+    var moveUp = function(){
+      grid[ robot.y ][ robot.x ] = null;
+      if (robot.y > 0){ robot.y -= 1; console.log('moving up');}
+      grid[ robot.y ][ robot.x ] = 'X';
+    }
+
+    var moveDown = function(){
+      grid[ robot.y ][ robot.x ] = null;
+      if (robot.y < length-1){ robot.y += 1; console.log('moving down');}
+      grid[ robot.y ][ robot.x ] = 'X';
+    }
+
+    var moveLeft = function(){
+      grid[ robot.y ][ robot.x ] = null;
+      if (robot.x > 0){ robot.x -= 1; console.log('moving left');}
+      grid[ robot.y ][ robot.x ] = 'X';
+    }
+
+    var moveRight = function(){
+      grid[ robot.y ][ robot.x ] = null;
+      if (robot.x < length-1){ robot.x += 1; console.log('moving right');}
+      grid[ robot.y ][ robot.x ] = 'X';
+    }
+
+    var move = function(string){
+      if (string === 'up'){ moveUp();}
+      if (string === 'down'){ moveDown();}
+      if (string === 'left'){ moveLeft();}
+      if (string === 'right'){ moveRight();} 
+    }
+
     return {
-      grid: grid
+      game : {
+        getGrid: getGrid,
+        initRobot: initRobot,
+        move: move
+      }
     };
   });
